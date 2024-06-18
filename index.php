12bb1695e7322dc7f17b61523e4df6a750c099a7
Amadou <atidiane.camara@gmail.com>
2024-06-18 10:31:41 +0200

    example pdo

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
 
-$db = new PDO('mysql:host=database;charset=utf8', 'root', 'example');
+try {
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ];
+    $db = new PDO('mysql:host=database;dbname=demo_pdo;charset=utf8', 'root', 'example', $options);
 
-var_dump($db);
+    $sql = "SELECT * FROM `users`";
+
+    $stmt = $db->query($sql);
+
+    $users = $stmt->fetchAll();
+    var_dump($users);
+} catch (\PDOException $e) {
+    echo sprintf('La connexion a échoué avec les message %s', $e->getMessage());
+    die();
+}
